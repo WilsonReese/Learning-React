408d0de735e36400ac88d495d53d3210bcb898c7
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-04-25 16:42:46 +0000

    added stylesheet and practiced with styles

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { Text } from "react-native";
-import { Profile } from "./components/Profile/Profile.jsx"
+import { StyleSheet, Text, View } from "react-native";
+import { Profile } from "./components/Profile/Profile.jsx";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { style } from "./App.style";
 
 export default function App() {
   return (
-    <>
-      <Text>Anchor down.</Text>
-      <Text>Anchor down.</Text>
-      <Text>Anchor down.</Text>
-      <Profile/>
-      <Text>Changed this for the branch test.</Text>
-    </>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Text style={style.title}>Anchor down.</Text>
+        <View style={style.square}/>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
---
+++ App.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+  title: {
+    color: "red",
+    fontSize: 50,
+    marginTop: 50,
+  },
+
+  square: {
+    height: 100,
+    width: 100,
+    backgroundColor: "blue"
+  }
+});
